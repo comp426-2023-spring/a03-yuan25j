fb14821129f5d0e6ae961c9a23976b802371691b
John Yuan <89277444+yuan25j@users.noreply.github.com>
2023-03-20 19:25:11 -0400

    a

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -41,6 +41,17 @@ let shot = args._[0]
 try {
     console.log(JSON.stringify(rpsls(shot)));
 } catch (e) {
+    console.log(`Usage: node-rpsls [SHOT]
+    Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+    
+      -h, --help        display this help message and exit
+      -r, --rules       display the rules and exit
+    
+    Examples:
+      node-rpsls        Return JSON with single player RPSLS result.
+                        e.g. {"player":"rock"}
+      node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                        e.g {"player":"rock","opponent":"Spock","result":"lose"}`);
     console.log(`Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
     - Scissors CUTS Paper
     - Paper COVERS Rock
@@ -53,4 +64,5 @@ try {
     - Spock VAPORIZES Rock
     - Rock CRUSHES Scissors
     `);
+    process.exit(1);
 }
